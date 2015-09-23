84c8fdd5b7cfd4e6a8a147a144d2cd2df07c2ae7
Yin Wu <m849876@a4370430.federated.fds>
2015-09-23 15:19:08 -0700

    in the progress of adding optional parameters

--- property_management/diff_properties.py
+++ property_management/diff_properties.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python
 import sys
 import urllib2
+import argparse
+
 #import HTTPError
+
 
 from bs4 import BeautifulSoup
 
 def main(argv):
-	src_env = argv[0]
-	dst_env = argv[1]
+	parser = argparse.ArgumentParser(description='Diff properties between two environments.')
+	parser.add_argument('src_env', #metavar='N', type=str, nargs='*',
+                   help='source environment used in the diff')
+	parser.add_argument('dst_env', #metavar='dst', type=str, nargs='*',
+                   help='destination environment used in the diff')
+	parser.add_argument('--print_all', dest='accumulate', action='store_const',
+    	               const=True, default=False,
+        	           help='Print all properties')
+	args = parser.parse_args()
+	#print args.print_all
+	#print args.dst_env
+	#exit(0)
+
+	src_env = args.src_env
+	dst_env = args.dst_env
 	PROPERTY_PAGE = "8080/sdp/sdp-admin/admin-service/getPropertiesInfo"
 	src_prop = get_properties_by_url('http://{0}:{1}'.format(src_env, PROPERTY_PAGE))
 	dst_prop = get_properties_by_url('http://{0}:{1}'.format(dst_env, PROPERTY_PAGE))
 	merged_prop = merge_properties(src_prop, dst_prop)
-	print_properties_dict(merged_prop)
-	print "Total {0} properties found in source.".format(len(src_prop))
-	print "Total {0} properties found in destination.".format(len(dst_prop))
+	print_properties_dict(merged_prop) # print_all=args.print_all)
+	print "Total {0} properties found in source: {1}".format(len(src_prop), src_env)
+	print "Total {0} properties found in destination: {1}".format(len(dst_prop), dst_env)
 
 def get_properties_by_url(url):
 	'''returns a list of (proerpty: value) as a dict'''
@@ -31,19 +47,30 @@ def get_properties_by_url(url):
 def merge_properties(src, dst):
 	result = {}
 	for p in src:
-		result[p] = (src[p], dst.get(p, None))
+		result[p] = (src[p], dst.get(p, ''))
 	for p in dst:
-		result[p] = (src.get(p, None), dst[p])
+		result[p] = (src.get(p, ''), dst[p])
 	return result
 
-def print_properties_dict(properties):
+def print_properties_dict(properties, print_all=False):
 	LINE_DIVIDER = '==========================================='
-	for pro, val in properties.iteritems():
-		print LINE_DIVIDER
-		print "name     : {0}".format(pro) 
-		print "src value: {0}".format(val[0])
-		print "dst value: {0}".format(val[1])
+	for key, val in properties.iteritems():
+		if not is_filtered(key, val, print_all=print_all):
+			print LINE_DIVIDER
+			print "name     : {0}".format(key) 
+			print "src value: {0}".format(val[0])
+			print "dst value: {0}".format(val[1])
 	print LINE_DIVIDER
+
+def is_filtered(prop_name, value, print_all=False):
+	'''returns True if the prop_name is supposed to be filtered in the final result. False otherwise'''
+	if not print_all:
+		if value[0] == value[1]:
+			return True #ignore the values that matches
+		else:
+			return False
+	else:
+		return False
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
